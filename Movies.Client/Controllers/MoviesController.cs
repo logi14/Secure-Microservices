a973a4e12c7146d4f591e0b25235f9d5e630fc41
Piotr Sidor <piotr.sid14@gmail.com>
2022-02-11 13:31:21 +0100

    Implementation of MVC Client Application For Consuming API Resources.

---
+++ Movies.Client/Controllers/MoviesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Client.ApiServices;
+using Movies.Client.Models;
+
+namespace Movies.Client.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly IMovieApiService _movieApiService;
+
+        public MoviesController(IMovieApiService movieApiService)
+        {
+            _movieApiService = movieApiService ?? throw new ArgumentNullException(nameof(movieApiService));
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _movieApiService.GetMovies());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Genre,Rating,ReleaseDate,ImageUrl,Owner")] Movie movie)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Genre,Rating,ReleaseDate,ImageUrl,Owner")] Movie movie)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        { 
+            return View();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            return View();
+        }
+
+        private bool MovieExists(int id)
+        {
+            return true;
+        }
+    }
+}
