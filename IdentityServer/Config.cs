a9b1216b3454cea044a6789a0e99db039b1aff41
Piotr Sidor <piotr.sid14@gmail.com>
2022-02-11 14:07:27 +0100

    Set DI Registration and Pipline of IS4 MVC UI Pages, update config.cs for IS4 OpenId Connect with MVC Client and Test User, Register OpenID Connect Client

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,6 +1,9 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
 using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace IdentityServer
 {
@@ -18,6 +21,31 @@ namespace IdentityServer
                         new Secret("secret".Sha256())
                     },
                     AllowedScopes = { "movieAPI" }
+                },
+                new Client
+                {
+                    ClientId = "movies_mvc_client",
+                    ClientName = "Movies MVC Web App",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowRememberConsent = false,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:5002/singnin-oidc"
+                    },
+                    PostLogoutRedirectUris = new List<string>()
+                    {
+                        "https://localhost:5002/signout-callback-oidc"
+                    },
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256()),
+                    },
+                    AllowedScopes = new List<string>()
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+
                 }
             };
 
@@ -33,10 +61,25 @@ namespace IdentityServer
 
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
-            { };
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
 
         public static List<TestUser> TestUser =>
             new List<TestUser>
-            { };
+            { 
+                new TestUser
+                {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "piotr",
+                    Password = "piotr",
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "piotr"),
+                        new Claim(JwtClaimTypes.FamilyName, "sidor")
+                    }
+                }
+            };
     }
 }
